5ed732304979f70870669bb64a72a1d45b18499f
Faisal Nour <faisalskhalifa@gmail.com>
2021-05-25 01:05:48 -0400

    The Shoppies project is added

--- src/components/RecentProjects/index.js
+++ src/components/RecentProjects/index.js
@@ -7,6 +7,8 @@ import PasswordGenerator from "../../components/RecentProjects/PasswordGenerator
 import TourismApp from "../../components/RecentProjects/TourismApp.png"
 import WorkDayScheduler from "../../components/RecentProjects/WorkDayScheduler.png"
 import BurgerLogger from "../../components/RecentProjects/BurgerLogger.png"
+import TheShoppies from "../../components/RecentProjects/TheShoppies.png"
+
 
 export default function RecentProject() {
     return (
@@ -15,6 +17,9 @@ export default function RecentProject() {
                 <p id="recentProject" >RECENT PROJECTS</p>
                 <div id="projects" className="row">
                     <Project image={GoldieMohrLtd} name="Goldie Mohr Ltd" deployed="https://goldie-mohr-full.herokuapp.com/" repository="https://github.com/GoldieMohrProject/goldie-mohr" description="The purpose of this project is to create a new and highly improved website for the company Goldie Mohr Ltd. The website contains home, services & projects, products, about us, careers and contact us pages. It also provide a login features and access to additional pages for registered employees" />
+                    <Project image={TheShoppies} name="The Shoppies" deployed="https://theshoppies-faisal.herokuapp.com/" repository="https://github.com/faisalsnour/movieSearch" description="This single-page web application allows user to search for movies and nominate them accordingly. Users are able to have their nominated movies saved in their web broswers so they can see them saved when they visit the website again. They are only allowed to nominate up to five movies. A banner will be displayed when five movies are nominated. The application uses OMDB's API to obtain information about movies.
+
+" />
                     <Project image={TourismApp} name="Tourism Application" deployed="https://faisalsnour.github.io/Tourism-App/" repository="https://github.com/faisalsnour/Tourism-App" description="The purpose of this project is to build a web application that allows travellers to search for cities around the world and find useful information so they plan their trips accordingly. For each searched city, the application shows the date, current weather, 5-days weather forecast, local currency of the searched city against Canadian Dollar, popular destination, and google map" />
                     <Project image={LMS} name="Library Management System" deployed="https://library-management-system-lms.herokuapp.com/" repository="https://github.com/faisalsnour/library-management-system" description="To build a management system that helps in handling the primary functions of libraries like maintaining the database for entering new books, tracking books that have been borrowed by members and storing members data in secure database
 
@@ -26,6 +31,7 @@ export default function RecentProject() {
                     <Project image={BurgerLogger} name="Burger Logger" deployed="https://faisal-burgerlog.herokuapp.com/" repository="https://github.com/faisalsnour/BurgerLogger" description="To create a burger logger using MySQL, Node, Express, Handlebars, ORM and Heroku to deploye the application. The application allows users to enter burger names into the database, validates user's entry to ensure a burger name is entered and displays the entered burger on the left side of the page, waiting to be devoured. Once the user clicks the devour button, the burger will move to the right side of the page
 
 " />
+
 
 
                 </div>
