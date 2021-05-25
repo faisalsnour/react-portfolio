53e7c468f6072fc4a4b950285282728e14a73267
Faisal Nour <faisalskhalifa@gmail.com>
2021-05-25 00:38:13 -0400

    TourismApp project is added

--- src/components/RecentProjects/index.js
+++ src/components/RecentProjects/index.js
@@ -13,6 +13,10 @@ export default function RecentProject() {
             <div className="container">
                 <div id="projects" className="row">
                     <Project image={GoldieMohrLtd} name="Goldie Mohr Ltd" deployed="https://goldie-mohr-full.herokuapp.com/" repository="https://github.com/GoldieMohrProject/goldie-mohr" description="The purpose of this project is to create a new and highly improved website for the company Goldie Mohr Ltd. The website contains home, services & projects, products, about us, careers and contact us pages. It also provide a login features and access to additional pages for registered employees" />
+                    <Project image={TourismApp} name="Tourism Application" deployed="https://faisalsnour.github.io/Tourism-App/" repository="https://github.com/faisalsnour/Tourism-App" description="The purpose of this project is to build a web application that allows travellers to search for cities around the world and find useful information so they plan their trips accordingly. For each searched city, the application shows the date, current weather, 5-days weather forecast, local currency of the searched city against Canadian Dollar, popular destination, and google map" />
+                    <Project image={LMS} name="Demo project" deployed="https://goldie-mohr-full.herokuapp.com/" repository="https://github.com/GoldieMohrProject/goldie-mohr" description="abc abc abc abc abc abc abc abc abc abc abc abc" />
+                    <Project image={PasswordGenerator} name="Demo project" deployed="https://goldie-mohr-full.herokuapp.com/" repository="https://github.com/GoldieMohrProject/goldie-mohr" description="abc abc abc abc abc abc abc abc abc abc abc abc" />
+                    <Project image={WorkDayScheduler} name="Demo project" deployed="https://goldie-mohr-full.herokuapp.com/" repository="https://github.com/GoldieMohrProject/goldie-mohr" description="abc abc abc abc abc abc abc abc abc abc abc abc" />
 
                 </div>
             </div>
