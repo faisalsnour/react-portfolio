70ff6f45bab75ddb09558f4f30b39aa6bc9acb0e
Faisal Nour <faisalskhalifa@gmail.com>
2021-05-27 23:54:39 -0400

    inital contact component is created

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,9 @@
 import "./style.css"
 import React, { useState, useRef } from "react"
+
+
 import { Link } from "react-router-dom";
+import { useEffect } from "react/cjs/react.development";
 
 
 
@@ -8,13 +11,16 @@ export default function Home() {
 
     const [word, setWord] = useState("Know more")
 
+
     return (
         <>
             <div id="hero">
                 <div id="knowMore" className="row">
                     <div className="col-7 col-xl-11">
                         <h2>Faisal Nour</h2>
-                        <p>A Front-end Developer and Certified Professional in Accessibility Core Competencies</p>
+                        <p>
+                            A Front-end Developer and Certified Professional in Accessibility Core Competencies
+                        </p>
                         <Link to="/about" id="btnKnowMore" type="button" className="btn btn-primary" onMouseLeave={() => { setWord("Know more") }} onMouseEnter={() => { setWord("Know more >") }}>{word}</Link>
                     </div>
                 </div>
