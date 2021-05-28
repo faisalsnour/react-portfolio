de3273b7ed08f15bb110740c24c985f5c9c69a83
Faisal Nour <faisalskhalifa@gmail.com>
2021-05-27 21:23:00 -0400

    updated resume file and download button added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,7 @@ import "./style.css"
 
 export default function About() {
     return (
-        <div id="content" class="card-body"  >
+        <div id="content" className="card-body"  >
             <p style={{ display: "inline;" }} >Faisal is a Front-end Developer and Certified Professional in
             Accessibility Core Competencies CPACC with experience in designing, developing,
             and testing websites and web-applications.
@@ -14,18 +14,18 @@ export default function About() {
                 </p>
             <p>
                 <ul style={{ listStyle: "none" }}>
-                    <i class="fas fa-square-full square"></i><li style={{ display: "inline" }}>York University IEP Bridging Program in System Analysis</li>
+                    <i className="fas fa-square-full square"></i><li style={{ display: "inline" }}>York University IEP Bridging Program in System Analysis</li>
                     <br />
-                    <i class="fas fa-square-full square"></i><li style={{ display: "inline" }}>Immigration Practitioner Diploma</li>
+                    <i className="fas fa-square-full square"></i><li style={{ display: "inline" }}>Immigration Practitioner Diploma</li>
                     <br />
-                    <i class="fas fa-square-full square"></i><li style={{ display: "inline" }}>Certified Professional in Accessibility Core Competencies </li>
+                    <i className="fas fa-square-full square"></i><li style={{ display: "inline" }}>Certified Professional in Accessibility Core Competencies </li>
                     <br />
-                    <i class="fas fa-square-full square"></i><li style={{ display: "inline" }}>Adobe Certified Expert - Business Practitioner</li>
+                    <i className="fas fa-square-full square"></i><li style={{ display: "inline" }}>Adobe Certified Expert - Business Practitioner</li>
                 </ul>
             </p>
             <p>Faisal has solid knowledge about HTML5, CSS, SASS, GIT, SQL, JavaSript, BootStrap, React JS,
                     Express.JS, RESTful API, Adobe Experience Manager, Agile Development, Jira, Adobe Photoshop, InVision and Sketch</p>
-            <p style={{ marginTop: "10px", marginLeft: "0px", fontWeight: "bold" }}><a id="pdfResume" href={Resume} download="FaisalNour-Resume">Download resume</a></p>
+            <p>Download Resume <a id="pdfResume" href={Resume} download="FaisalNour-Resume"><i className="fas fa-download fa-2x" id="resumeDownload"></i></a></p>
 
         </div>
 
