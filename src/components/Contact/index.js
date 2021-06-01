d202fedbf2bc37bbaf156cf8159a04446b677e05
Faisal Nour <faisalskhalifa@gmail.com>
2021-06-01 00:23:10 -0400

    three states have been created for contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,12 @@
 import "./style.css"
+import React, { useState, useRef } from "react"
 
 export default function Contact() {
+
+    const [name, setName] = useState("")
+    const [senderEmail, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+
     return (
         <>
             <div className="container">
@@ -16,7 +22,7 @@ export default function Contact() {
                         <input type="email" className="form-control" id="emailControlInput" placeholder="Enter your email address" />
                     </div>
                     <div className="mb-3">
-                        <label for="messageControlInput" className="form-label">Example textarea</label>
+                        <label for="messageControlInput" className="form-label">Message</label>
                         <textarea className="form-control" id="messageControlInput" rows="6" placeholder="Enter your message here"></textarea>
                     </div>
                     <div className="mb-3" style={{ textAlign: "center" }}>
