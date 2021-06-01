9c80220f64948f0cb0c8f6d6527de774ab68041a
Faisal Nour <faisalskhalifa@gmail.com>
2021-06-01 12:25:46 -0400

    sendEmail function is built

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,7 @@
 import "./style.css"
 import React, { useState, useRef } from "react"
+import emailjs from 'emailjs-com';
+
 
 export default function Contact() {
 
@@ -26,62 +28,57 @@ export default function Contact() {
     }
 
 
-    senderMessage = (event) => {
+    function sendEmail(event) {
         event.preventDefault();
 
-        const templateParams = {
-            from_name: { senderName } + " (" + { senderEmail } + ")",
-            to_name: "Faisal",
-            message: { senderMessage }
-        };
+        // const templateParams = {
+        //     user_name: senderName,
+        //     user_email: senderEmail,
+        //     message: senderMessage
+        // };
 
-        // emailjs
-        //     .send("gmail", "portfoliositecontact", templateParams, "template_48jjry4")
-        //     .then(
-        //         function (response) {
-        //             toast.success("Your message has successfully sent!", {
-        //                 position: toast.POSITION.BOTTOM_CENTER
-        //             });
-        //             console.log("SUCCESS!", response.status, response.text);
-        //         },
-        //         function (err) {
-        //             toast.error("Your message was not able to be sent");
-        //         }
-        //     );
+        emailjs
+            .sendForm("service_7wfzojs", "contact_form", event.target, "user_nOyVexTohkJUOhOVmUL5g"
+            )
+            .then(function (response) {
+                console.log('SUCCESS!', response.status, response.text);
+            }, function (error) {
+                console.log('FAILED...', error);
+            });
+
     }
 
-    function sendEmail() {
-        console.log(`name = ${senderName}, email =${senderEmail}, message =${senderMessage}`)
-    }
+    // function sendEmail() {
+    //     console.log(`name = ${senderName}, email =${senderEmail}, message =${senderMessage}`)
+    // }
 
     return (
         <>
             <div className="container">
                 <p id="contactMe" >SEND A MESSAGE</p>
                 <div id="line" ></div>
-                <form>
+                <form onSubmit={sendEmail}>
                     <div className="row">
                         <div className="mb-3">
                             <label for="senderName" className="form-label">Full Name</label>
-                            <input type="text" className="form-control" id="senderName" onChange={handleChange} placeholder="Enter your full name" required />
+                            <input type="text" name="user_name" className="form-control" id="senderName" onChange={handleChange} placeholder="Enter your full name" required />
                         </div>
                         <div className="mb-3">
                             <label for="senderEmail" className="form-label">Email address</label>
-                            <input type="email" className="form-control" id="senderEmail" onChange={handleChange} placeholder="Enter your email address" required />
+                            <input type="email" name="user_email" className="form-control" id="senderEmail" onChange={handleChange} placeholder="Enter your email address" required />
                         </div>
                         <div className="mb-3">
                             <label for="senderMessage" className="form-label">Message</label>
-                            <textarea className="form-control" id="senderMessage" rows="6" onChange={handleChange} placeholder="Enter your message here" required ></textarea>
+                            <textarea className="form-control" name="message" id="senderMessage" rows="6" onChange={handleChange} placeholder="Enter your message here" required ></textarea>
                         </div>
                         <div className="mb-3" style={{ textAlign: "center" }}>
-                            <button type="submit" onSubmit={sendEmail} class="btn" id="btnSend">Primary</button>
-                        </div>
-                        <div>
-                            <p>Name: {senderName}, Email: {senderEmail}, Message: {senderMessage}</p>
+                            <button type="submit" value="Send" name="submit" className="btn" id="btnSend" onclick={sendEmail}>Primary</button>
                         </div>
                     </div>
                 </form>
-
+                <div>
+                    <p>Name: {senderName}, Email: {senderEmail}, Message: {senderMessage}</p>
+                </div>
             </div>
 
         </>
