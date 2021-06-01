11f2dcc9fc1386e9bb628417e6efccc629fa504e
Faisal Nour <faisalskhalifa@gmail.com>
2021-05-31 23:01:35 -0400

    Title is added to contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,12 @@
+import "./style.css"
 
 export default function Contact() {
     return (
         <>
             <div className="container">
+                <p id="contactMe" >SEND A MESSAGE</p>
+                <div id="line" ></div>
                 <div className="row">
-
                     <div className="mb-3">
                         <label for="nameControlInput" className="form-label">Full Name</label>
                         <input type="text" className="form-control" id="nameControlInput" placeholder="Enter your full name" />
@@ -15,7 +17,7 @@ export default function Contact() {
                     </div>
                     <div className="mb-3">
                         <label for="messageControlInput" className="form-label">Example textarea</label>
-                        <textarea className="form-control" id="messageControlInput" rows="3"></textarea>
+                        <textarea className="form-control" id="messageControlInput" rows="6" placeholder="Enter your message here"></textarea>
                     </div>
                 </div>
 
