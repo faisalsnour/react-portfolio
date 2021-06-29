7a8fe8201e200f5798ee7ddb0a606d4d709d917f
Faisal Nour <faisalskhalifa@gmail.com>
2021-06-29 11:58:06 -0400

    email and phone number added to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,8 @@ export default function Footer() {
                     <div className="row">
                         <div className="col">
                             <div style={{ textAlign: "center", marginBottom: "15px", marginTop: "15px" }}>
-                                <p style={{ marginBottom: "15px" }}>Connect with me through</p>
+                                <p style={{ marginBottom: "10px" }}>Connect with me through</p>
+                                <p style={{ marginBottom: "10px" }}><a style={{ color: "white", textDecoration: "none" }} target="_blank" rel="noreferrer" href="mailto:faisalskhalifa@gmail.com">faisalskhalifa@gmail.com </a>| <span>(647) 309 - 5646</span></p>
                                 <ul style={{ padding: "0px" }}>
                                     <li style={{ display: "inline-flex", marginRight: "10px" }}><a aria-label="linkedIn" title="LinkedIn" href="https://www.linkedin.com/in/faisal-nour/" target="_blank" rel="noreferrer" className="button_social"><i id="linkedinlogo" className="fab fa-linkedin-in fa-2x" aria-hidden="true"></i> </a></li>
                                     <li style={{ display: "inline-flex", marginRight: "10px" }}><a aria-label="Github" name="github" title="Github" href="https://github.com/faisalsnour" target="_blank" rel="noreferrer" className="button_social"><i id="githublogo" className="fab fa-github fa-2x" aria-hidden="true"></i> </a></li>
